a990114395241ec4075b1c490266df5e2cd170cd
Joselu Aguilar <joselubiker@gmail.com>
2020-04-22 14:31:18 +0200

    Añade atributos en clase modelo

---
+++ src/main/java/Paquetaso/Pato.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paquetaso;
+
+/**
+ *
+ * @author josel
+ */
+public class Pato {
+    // Se añaden los atributos de la clase
+    private int patas;
+    private boolean volar;
+    
+}
